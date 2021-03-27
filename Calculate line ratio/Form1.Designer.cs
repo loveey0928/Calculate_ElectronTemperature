4be5ae495c98af6200ecec14cf025688b3f1e887
hjkwon0928@mju.ac.kr <hjkwon0928@mju.ac.kr>
2021-03-28 00:05:59 +0900

    초기 커밋.

---
+++ Calculate line ratio/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Calculate_line_ratio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileLoad = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.btnCalLineRatio = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileLoad
+            // 
+            this.btnFileLoad.Location = new System.Drawing.Point(25, 51);
+            this.btnFileLoad.Name = "btnFileLoad";
+            this.btnFileLoad.Size = new System.Drawing.Size(118, 23);
+            this.btnFileLoad.TabIndex = 0;
+            this.btnFileLoad.Text = "파일 불러오기";
+            this.btnFileLoad.UseVisualStyleBackColor = true;
+            this.btnFileLoad.Click += new System.EventHandler(this.fBtnFileLoad_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(158, 56);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(41, 12);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "파일명";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(12, 100);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowTemplate.Height = 23;
+            this.dgv1.Size = new System.Drawing.Size(776, 338);
+            this.dgv1.TabIndex = 2;
+            // 
+            // btnCalLineRatio
+            // 
+            this.btnCalLineRatio.Location = new System.Drawing.Point(471, 51);
+            this.btnCalLineRatio.Name = "btnCalLineRatio";
+            this.btnCalLineRatio.Size = new System.Drawing.Size(145, 23);
+            this.btnCalLineRatio.TabIndex = 3;
+            this.btnCalLineRatio.Text = "Calculate lineRatio";
+            this.btnCalLineRatio.UseVisualStyleBackColor = true;
+            this.btnCalLineRatio.Click += new System.EventHandler(this.btnCalLineRatio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalLineRatio);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnFileLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFileLoad;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button btnCalLineRatio;
+    }
+}
+
---
+++ Calculate line ratio/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Microsoft.Office.Interop.Excel;
+
+namespace Calculate_line_ratio
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtA = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fBtnFileLoad_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btn_aFileLoad_Clicked");
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = string.Empty;
+
+
+                filePath = dialog.FileName;
+                Console.WriteLine(String.Format("{0} was imported by btn_aFileLoad_Click", filePath));
+
+                if (filePath.EndsWith(".csv"))
+                    dtA = CSVconvertToDataTable(filePath, "dtA");   // 이 함수 쓰려면 csv 파일 맨 윗줄의 cell이 하나라도 비어있으면 안됨.
+                else if (filePath.EndsWith(".xlsx") || filePath.EndsWith(".xls"))
+                { 
+                    //dtA = Xlsx_xlsConvertToDataTable(filePath, "dtA");
+                    dtA = exceldata(filePath);
+                    dgv1.DataSource = dtA;
+
+                }
+            }
+        }
+
+        private DataTable CSVconvertToDataTable(string filePath, string dtType)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                string firstLine = lines[0]; // 2
+
+                string[] headerLabels = firstLine.Split(',');
+
+                short columnIndex = 0;
+                foreach (string headerWord in headerLabels)   // create header of Datatale
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                    columnIndex++;
+                }
+
+
+                for (int lineNum = 1; lineNum < lines.Length; lineNum++) // fill data to Datatale // 3
+                {
+                    string[] dataWords = lines[lineNum].Split(',');
+                    DataRow dr = dt.NewRow();
+                    columnIndex = 0;
+                    foreach (string headerWord in headerLabels)
+                    {
+                        dr[headerWord] = dataWords[columnIndex++];
+                    }
+                    dt.Rows.Add(dr);
+                }
+
+                if (dt.Rows.Count > 0)
+                {
+                    if (dtType == "dtA")
+                        dgv1.DataSource = dt;
+                    //else if (dtType == "dtB")
+                    //    dgv_2.DataSource = dt;
+                }
+            }
+            return dt;
+        }
+
+        private DataTable Xlsx_xlsConvertToDataTable(string filePath, string dtType)
+        {
+
+            string dirName = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+            string fileExtension = Path.GetExtension(filePath);
+            string pathConn = string.Empty;
+            string excelsql = string.Empty;
+            try
+            {
+                switch (fileExtension)
+                {
+                    case ".xls":
+                        pathConn = $@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={filePath};" + "Extended Properties=\"Excel 8.0; HDR=Yes; IMEX=1\"";
+                        excelsql = "SELECT * FROM [WorkSheet$]";
+                        break;
+
+                    case ".xlsx":
+                        pathConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=YES'";
+                        excelsql = @"select * from[WorkSheet$]";
+                        break;
+
+                    case ".csv":
+                        pathConn = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dirName};" + "Extended Properties=\"text; HDR=Yes; IMEX=1; FMT=Delimited\"";
+                        excelsql = $"SELECT * FROM [{fileName}]";
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("오류의 건:" + e.Message);
+            }
+            OleDbConnection conn = new OleDbConnection(pathConn);
+            OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(excelsql, conn);
+            DataSet excelDs = new DataSet();
+            _ = myDataAdapter.Fill(excelDs);
+            DataTable excelTable = excelDs.Tables[0];
+            if (dtType == "dtA")
+                dgv1.DataSource = excelTable;
+            //else if (dtType == "dtB")
+            //    dgv_2.DataSource = excelTable;
+            return excelTable;
+
+        }
+
+        public static DataTable exceldata(string filePath)
+        {
+            DataTable dtexcel = new DataTable();
+            bool hasHeaders = false;
+            string HDR = hasHeaders ? "Yes" : "No";
+            string strConn;
+            if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + HDR + ";IMEX=0\"";
+            else
+                strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=" + HDR + ";IMEX=0\"";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            //Looping Total Sheet of Xl File
+            /*foreach (DataRow schemaRow in schemaTable.Rows)
+            {
+            }*/
+            //Looping a first Sheet of Xl File
+            DataRow schemaRow = schemaTable.Rows[0];
+            string sheet = schemaRow["TABLE_NAME"].ToString();
+            if (!sheet.EndsWith("_"))
+            {
+                string query = "SELECT  * FROM [" + "sheet1" + "]";
+                OleDbDataAdapter daexcel = new OleDbDataAdapter(query, conn);
+                dtexcel.Locale = CultureInfo.CurrentCulture;
+                daexcel.Fill(dtexcel);
+            }
+
+            conn.Close();
+            return dtexcel;
+
+        }
+
+        //private void btnCalLineRatio_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show(dtA.Columns.Count.ToString());
+
+        //    MessageBox.Show(dtA.Columns[1].ToString());
+
+        //    int dtA_ColumnCount = dtA.Columns.Count - 1;
+
+        //    for (int x1=1 ; x1<=dtA_ColumnCount ; x1++) 
+        //    {
+        //        for (int x2 = 1 ; x2 <= dtA_ColumnCount ; x2++)
+        //        {
+        //            if (x2 != x1)  // column이 동일하면 그냥 skip
+        //            {
+        //                string newColumnName = dtA.Columns[x1] +"nm"+"/" + dtA.Columns[x2]+"nm";
+        //                if (!dtA.Columns.Contains(newColumnName))
+        //                {
+        //                    dtA.Columns.Add(newColumnName);
+        //                }
+
+                       
+
+        //                for (int rowNum = 0; rowNum <= dtA.Rows.Count - 1; rowNum++)
+        //                {
+        //                    double dx1Value = double.Parse(dtA.Rows[rowNum][x1].ToString());
+        //                    double dx2Value = double.Parse(dtA.Rows[rowNum][x2].ToString());
+
+        //                    dtA.Rows[rowNum][newColumnName] = dx1Value / dx2Value;
+        //                }
+                        
+
+        //            }
+        //        }
+        //    }
+
+        //    dgv1.DataSource = dtA;
+
+        //}
+
+        private void btnCalLineRatio_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dtA.Columns.Count.ToString());
+
+            MessageBox.Show(dtA.Columns[1].ToString());
+
+            int dtA_ColumnCount = dtA.Columns.Count - 1;
+
+            for (int x1 = 1; x1 <= dtA_ColumnCount; x1++)
+            {
+                for (int x2 = 1; x2 <= dtA_ColumnCount; x2++)
+                {
+                    if (x2 != x1)  // column이 동일하면 그냥 skip
+                    {
+                        string newColumnName = dtA.Columns[x1] + "nm" + "/" + dtA.Columns[x2] + "nm";
+                        if (!dtA.Columns.Contains(newColumnName))
+                        {
+                            dtA.Columns.Add(newColumnName);
+                        }
+
+
+
+                        for (int rowNum = 1; rowNum <= dtA.Rows.Count - 1; rowNum++)
+                        {
+                            double dx1Value = double.Parse(dtA.Rows[rowNum][x1].ToString());
+                            double dx2Value = double.Parse(dtA.Rows[rowNum][x2].ToString());
+
+                            dtA.Rows[rowNum][newColumnName] = dx1Value / dx2Value;
+                        }
+
+
+                    }
+                }
+            }
+
+            dgv1.DataSource = dtA;
+
+        }
+    }
+}
